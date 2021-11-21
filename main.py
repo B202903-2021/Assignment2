211404cc8feb21e53ebb9aa7da003b91fbaf61f6
B202903 <email_not_provided>
2021-11-21 21:31:04 +0000

    Create function to count species, and protein sequences

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("\n Imported sys\n")
 import os
 print("\n Imported os\n")
 import shutil
-:w::print("\n Imported shutiln\n")
+print("\n Imported shutiln\n")
 
 #Import edirect module
 sys.path.insert(1, os.path.dirname(shutil.which('xtract')))
@@ -107,4 +107,46 @@ def Float_check(IC_input) : #Checks for float
         return True
     else:
         return False
+
+
+#Create Species Dic with count per species
+def dict_species(fasta_input):
+    with open(f'{fasta_input}', 'r') as i:
+        empty_list = []
+        empty_dic = {}
+        for line in i:
+            if line[0] == ">":
+                s = line
+                pattern = "\[(.*?)\]"
+                substring = re.search(pattern, s).group(1)
+                if substring not in empty_list:
+                    empty_dic[substring] = 1
+                    empty_list += [substring]
+                else:
+                    empty_dic[substring] += 1
+        return empty_dic
+
+# Show count of each specieis
+def count_per_species(fasta_input):
+    print("\n Counts per species \n")
+    for species, count in dict_species(fasta_input).items():
+        print(f"{species} and {count}")
+# Summary count_per_sepcieis
+#Count Species
+def count_species(fasta_input):
+    with open(f'{fasta_input}', 'r') as i:
+        empty_list = []
+        empty_dic = {}
+        count = 0
+        for line in i:
+            if line[0] == ">":
+                s = line
+                pattern = "\[(.*?)\]"
+                substring = re.search(pattern, s).group(1)
+                if substring not in empty_list:
+                    empty_list += [substring]
+                    count += 1
+
+        return count
+
 
