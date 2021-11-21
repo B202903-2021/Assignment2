967e599edb8df4e2d2312f497126a912d9fb8819
B202903 <email_not_provided>
2021-11-21 21:25:28 +0000

    Adding neccesary import modules

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 #File called main.py
+import sys
+print("\n Imported sys\n")
+import os
+print("\n Imported os\n")
+import shutil
+print("\n Imported shutiln\n")
+
+#Import edirect module
+sys.path.insert(1, os.path.dirname(shutil.which('xtract')))
+import edirect
+print("\n Imported edirect\n")
+import subprocess
+print("\n Imported subprocess\n")
+import re
+
+from PIL import Image
+print("\n Imported Image\n")
+from os.path import exists # Check if the file exists
+print("\n Imported exists from os.path\n")
+import pandas as pd
+print("\n Imported pandas as pd \n")
+
